976c2863644461ff157e80309d30ae1adf7ec0bb
Jens Graversen <jens@cetg.dk>
2024-02-27 12:37:20 +0100

    Loads a collection of shapes as a VAO vector

--- Matricing/Main.cpp
+++ Matricing/Main.cpp
@@ -54,35 +54,9 @@ int main()
 
 	Shader program("uniform", "ourColor");
 	cout << endl;
-	VAO cube("C:/VSC_PRO_B/OpenGL/resources/shapes/cube");
 	program.use();
 
-	unsigned int texture;
-	glGenTextures(1, &texture);
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture);
-
-	// set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-	// load and generate the texture
-	stbi_set_flip_vertically_on_load(true);
-	int width; int height; int nrChannels;
-	unsigned char* data = stbi_load("C:/VSC_PRO_B/OpenGL/resources/textures/red_brick_wall.jpg", &width, &height, &nrChannels, STBI_rgb_alpha);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
+	vector<VAO> cubes = readCollection("C:/VSC_PRO_B/OpenGL/resources/collections/proof.col");
 
 	glEnable(GL_DEPTH_TEST);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -101,27 +75,21 @@ int main()
 
 		// Creating a projection:
 		mat4 proj = perspective(radians(45.0f), SCR_HEIGHT > 0 ? (float)(SCR_WIDTH / SCR_HEIGHT) : 1, 0.1f, 1000.0f);
+		program.setMat4("proj", proj);
 		// Moving the camera:
 		mat4 view = mat4(1.0f);
 		view = translate(view, vec3(0.0f, 0.0f, -15.0f));
-
-		program.setMat4("proj", proj);
 		program.setMat4("view", view);
 
-		for (int i = 0; i < 6; i++)
+		// Moving the model around:
+		mat4 model = mat4(1.0f);
+		model = rotate(model, (float)glfwGetTime() * radians(10.0f), vec3(1.0f, 1.0f, 0.0f));
+		program.setMat4("model", model);
+
+		for (int i = 0; i < cubes.size(); i++)
 		{
-			// Moving the model around:
-			mat4 model = mat4(1.0f);
-			model = translate(model, vec3(i - 3, (float)i / 2, -5.0f));
-			model = rotate(model, (float)glfwGetTime() * radians(20.0f), vec3(1.0f, 0.0f, 0.0f));
-			model = scale(model, vec3(1.0f, i + 1, 1.0f));
-
-			program.setMat4("model", model);
-
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, texture);
-
-			glDrawElements(GL_TRIANGLES, cube.nInd, GL_UNSIGNED_INT, NULL);
+			cubes[i].bind();
+			glDrawElements(GL_TRIANGLES, cubes[i].nInd, GL_UNSIGNED_INT, NULL);
 		}
 
 
